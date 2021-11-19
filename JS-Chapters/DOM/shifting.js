fa4df24aef07a14c2b3775ec12ede93c372c21b0
Jeffry Babb <ahuimanu@gmail.com>
2021-11-18 22:21:20 -0600

    updated node packages

--- JS-Chapters/DOM/shifting.js
+++ JS-Chapters/DOM/shifting.js
@@ -2,19 +2,30 @@ function intervals(){
     setInterval(doStuff, 1000);
 }
 
+function makeColors(area){
+    let r = Math.random() * 255;
+    console.log(`random red is: ${r}`);
+    let g = Math.random() * 255;
+    let b = Math.random() * 255;
+    let a = Math.random();
+    area.style.backgroundColor = `rgba(${r}, ${g}, ${b}, ${a})`; 
+}
+
 function doStuff(){
 
     let timeInfoElement = document.getElementById("timeinfo");
     timeInfoElement.innerHTML = `${new Date(Date.now()).toString()}`;
 
     let areas = document.querySelectorAll(".grid-container > div");
+    areas.forEach(makeColors);
 
-    areas.forEach(area => {
-        let r = Math.random() * 255;
-        console.log(`random red is: ${r}`);
-        let g = Math.random() * 255;
-        let b = Math.random() * 255;
-        let a = Math.random();
-        area.style.backgroundColor = `rgba(${r}, ${g}, ${b}, ${a})`;
-    });
+
+    // areas.forEach(area => {
+    //     let r = Math.random() * 255;
+    //     console.log(`random red is: ${r}`);
+    //     let g = Math.random() * 255;
+    //     let b = Math.random() * 255;
+    //     let a = Math.random();
+    //     area.style.backgroundColor = `rgba(${r}, ${g}, ${b}, ${a})`;
+    // });
 }
