775284a0ef54d04073fd84c55a17f3a7fac4f856
Jeffry Babb <ahuimanu@gmail.com>
2022-11-13 16:23:42 -0600

    express form handling

--- FALL2022/forms/app.js
+++ FALL2022/forms/app.js
@@ -3,21 +3,39 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var bodyParser = require('body-parser');
+var multer = require('multer');
+var upload = multer();
 
+// create routes
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+// init express
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 
+//configure middleware
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+
+// for parsing application/json
+// app.use(bodyParser.json()); 
+
+// for parsing application/xwww-
+app.use(bodyParser.urlencoded({ extended: true })); 
+//form-urlencoded
+
+// for parsing multipart/form-data
+// app.use(upload.array()); 
+// app.use(express.static('public'));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
--- FALL2022/forms/routes/index.js
+++ FALL2022/forms/routes/index.js
@@ -3,7 +3,13 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Weather Form' });
+});
+
+router.post('/form', function(req, res, next) {
+  console.log(req.body);
+  res.render('formresponse', {bruh: req.body.stationid})
+  // res.send(`BRUH recieved your request! ${req.body.stationid}`);
 });
 
 module.exports = router;
